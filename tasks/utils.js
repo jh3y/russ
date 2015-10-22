aeabf4670c77b01feb11b244c1e7379fad35cb49
jh3y <jh3y@users.noreply.github.com>
2015-10-22 14:28:32 +0100

    Implement basic stylus task

---
+++ tasks/utils.js
@@ -0,0 +1,30 @@
+(function(){
+  var fs    = require('fs'),
+    utils = {
+      readFiles: function(filePaths, callBack) {
+        var files = {},
+          errors  = {};
+        filePaths.forEach(function(filePath, idx) {
+          console.log(filePath, idx);
+          fs.readFile(filePath, function(err, fileData){
+            if (err) {
+              errors[filePath] = err;
+            } else {
+              files[filePath]  = fileData;
+            }
+            if (idx + 1 === filePaths.length) {
+              callBack(errors, files);
+            }
+          });
+        });
+      },
+      concatFiles: function(files) {
+        var result;
+        for (var file in files) {
+          result += files[file];
+        }
+        return result;
+      }
+    };
+  module.exports = utils;
+}(this));
