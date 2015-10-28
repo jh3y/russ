6ec1d059b605723ce4161245f4dc83ff18129779
jh3y <jh3y@live.co.uk>
2015-10-28 02:41:30 +0000

    Add more generic watching with use of utils.getArgs function

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,12 +1,19 @@
 (function(){
   var fs    = require('fs'),
     compile = require('./compile'),
+    utils   = require('./utils'),
+    compiler = {
+      compile: compile
+    },
     watch   = function() {
-      console.log(process.argv  );
-      fs.watch('src/stylus', function(event, filename) {
+      var args = utils.getArgs(process.argv);
+      if (args.name) {
+        console.log(args.name, 'watcher started!');
+      }
+      fs.watch(args.dir, function(event, filename) {
         if (filename) {
           console.log(filename, 'changed!');
-          compile();
+          compiler[args.exec]();
         }
       });
     };
