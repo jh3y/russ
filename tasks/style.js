beeb8897d6dd924e79f141993466ee95e6486b99
jh3y <jh3y@live.co.uk>
2015-11-03 11:16:11 +0000

    Refactor readFiles to have concatenated flag also

--- tasks/style.js
+++ tasks/style.js
@@ -1,35 +1,31 @@
 (function(){
-  var glob  = require('glob'),
-    fs      = require('fs'),
+  var fs    = require('fs'),
     stylus  = require('stylus'),
     postcss = require('postcss'),
     utils   = require('./utils'),
     compile = function() {
-      glob('./src/stylus/{style,*}.stylus', {nosort: true}, function(err, files){
-        utils.readFiles(files, function(actualFiles) {
-          var styleContent = utils.concatFiles(actualFiles, files);
-          stylus(styleContent)
-            .render(function(err, css) {
-              var outputFile = function(styles) {
-                fs.writeFileSync('public/test.css', styles);
-              },
-                minifyFile = function(styles) {
-                  console.info('MINIFYING.... brrr, greger');
-                  postcss([ require('autoprefixer') ])
-                    .process(css, { from: 'public/test.css', to: 'test.min.css' })
-                    .then(function (result) {
-                        fs.writeFileSync('public/test.min.css', result.css);
-                    });
-                };
-              if (!fs.existsSync('./public/')){
-                fs.mkdirSync('./public/');
-              }
-              // Try prepending the license here.
-              css = utils.license(css);
-              outputFile(css);
-              minifyFile();
-            });
-        });
+      utils.readFiles('./src/stylus/{style,*}.stylus', true, function(files, paths) {
+        stylus(files)
+          .render(function(err, css) {
+            var outputFile = function(styles) {
+              fs.writeFileSync('public/test.css', styles);
+            },
+              minifyFile = function(styles) {
+                console.info('MINIFYING.... brrr, greger');
+                postcss([ require('autoprefixer') ])
+                  .process(css, { from: 'public/test.css', to: 'test.min.css' })
+                  .then(function (result) {
+                      fs.writeFileSync('public/test.min.css', result.css);
+                  });
+              };
+            if (!fs.existsSync('./public/')){
+              fs.mkdirSync('./public/');
+            }
+            // Try prepending the license here.
+            css = utils.license(css);
+            outputFile(css);
+            minifyFile();
+          });
       });
     };
   if (require.main === module) {
