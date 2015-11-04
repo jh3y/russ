b82612fb749088dc519598b28a579420e2414e9f
jh3y <jh3y@live.co.uk>
2015-11-04 04:09:25 +0000

    Refactor style compilation file a little and include nano for minification

--- tasks/style.js
+++ tasks/style.js
@@ -2,24 +2,27 @@
   var fs    = require('fs'),
     stylus  = require('stylus'),
     postcss = require('postcss'),
+    prefix  = require('autoprefixer'),
+    nano    = require('cssnano'),
     utils   = require('./utils'),
     compile = function() {
       utils.readFiles('./src/stylus/{style,*}.stylus', true, function(files, paths) {
         stylus(files)
           .render(function(err, css) {
             var outputFile = function(styles) {
-              fs.writeFileSync('public/test.css', styles);
+              fs.writeFileSync('public/css/test.css', styles);
             },
               minifyFile = function(styles) {
-                console.info('MINIFYING.... brrr, greger');
-                postcss([ require('autoprefixer') ])
-                  .process(css, { from: 'public/test.css', to: 'test.min.css' })
+                var autoOpts = {};
+                postcss([ prefix(autoOpts), nano({}) ])
+                  .process(css, { from: 'public/css/test.css', to: 'test.min.css' })
                   .then(function (result) {
-                      fs.writeFileSync('public/test.min.css', result.css);
+                      result.css = utils.license(result.css);
+                      fs.writeFileSync('public/css/test.min.css', result.css);
                   });
               };
-            if (!fs.existsSync('./public/')){
-              fs.mkdirSync('./public/');
+            if (!fs.existsSync('./public/css/')){
+              fs.mkdirSync('./public/css/');
             }
             // Try prepending the license here.
             css = utils.license(css);
