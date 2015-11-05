5b6cee63ac08b5a3154f0867af6b50e1b0899ad1
jh3y <jh3y@live.co.uk>
2015-11-05 14:11:22 +0000

    Add extra jade file to ensure behaving properly and add arguments for when publishing

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -6,11 +6,14 @@
     dest    = config.paths.destinations,
     opts    = config.pluginOpts,
     utils   = require('./utils'),
+    args    = utils.getArgs(process.argv),
     compile = function() {
       utils.readFiles(src.scripts, true, function(files) {
         var scripts = coffee.compile(files, opts.coffee);
         utils.checkPath(dest.scripts);
-        scripts = utils.license(scripts);
+        if (args.licensed) {
+          scripts = utils.license(scripts);
+        }
         fs.writeFileSync(dest.scripts + config.name + '.js', scripts);
       });
     };
